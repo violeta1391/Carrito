59c4dfee59e11f0b29678e02cc6bf076cb38f11b
Violeta Rodriguez <vio.ayelen@gmail.com>
2022-04-01 19:58:10 -0300

    borre archivos

---
+++ carrito/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { Grid, Button, TextField, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Grid>Edit <code>src/App.js</code> and save to reload.</Grid> 
+          
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
